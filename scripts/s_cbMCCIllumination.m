e3c83bc06306d4b0de628c333ad9d17a1a24ac5c
Brian Wandell <wandell@stanford.edu>
2021-01-18 15:10:31 -0800

    Draft of how we are making figures for the final paper.

--- scripts/s_cbMCCIllumination.m
+++ scripts/s_cbMCCIllumination.m
@@ -40,4 +40,70 @@ for ii=1:numel(dataFiles)
     sensorWindow(sensor);
 end
 
+%% Let's make some images of the MCC in different positions
+
+% cp = sensorGet(sensor,'chart corner points');
+sensor = ieGetObject('sensor');
+
+cp = sensorGet(sensor,'corner points')
+if isempty(cp)
+    cp = chartCornerpoints(sensor,false);  % Get the corner points
+    sensor = sensorSet(sensor,'corner points',cp);
+    ieReplaceObject(sensor);
+end
+[rects,mLocs,pSize] = chartRectangles(cp,4,6,0.5);  % MCC parameters
+chartRectsDraw(sensor,rects);
+
+fullData = false;
+dataType = 'dv';
+delta = round(pSize(1)*0.5);
+
+% rgb are the 24x3 values (average) from the sensor at the macbeth positions.
+rgb = chartRectsData(sensor,mLocs,delta,fullData,dataType);
+
 %%
+sensorRed = ieGetObject('sensor');
+if isempty(cp)
+    cp = chartCornerpoints(sensorRed,false);  % Get the corner points
+    sensor = sensorSet(sensorRed,'corner points',cp);
+    ieReplaceObject(sensorRed);
+end
+[rects,mLocs,pSize] = chartRectangles(cp,4,6,0.5);  % MCC parameters
+chartRectsDraw(sensorRed,rects);
+
+fullData = false;
+dataType = 'dv';
+delta = round(pSize(1)*0.5);
+
+% rgb are the 24x3 values (average) from the sensor at the macbeth positions.
+rgb2 = chartRectsData(sensorRed,mLocs,delta,fullData,dataType);
+sensorRed = sensorSet(sensorRed,'corner points',cp);
+
+%%
+ieNewGraphWin; plot(rgb,rgb2,'o'); identityLine
+xlabel('Center'); ylabel('Red wall');
+
+%%
+sensorGreen = ieGetObject('sensor');
+if isempty(cp)
+    cp = chartCornerpoints(sensorGreen,false);  % Get the corner points
+    sensor = sensorSet(sensorGreen,'corner points',cp);
+    ieReplaceObject(sensorGreen);
+end
+[rects,mLocs,pSize] = chartRectangles(cp,4,6,0.5);  % MCC parameters
+chartRectsDraw(sensorGreen,rects);
+
+fullData = false;
+dataType = 'dv';
+delta = round(pSize(1)*0.5);
+
+% rgb are the 24x3 values (average) from the sensor at the macbeth positions.
+rgb3 = chartRectsData(sensorGreen,mLocs,delta,fullData,dataType);
+sensorGreen = sensorSet(sensorGreen,'corner points',cp);
+
+%%
+ieNewGraphWin; plot(rgb,rgb3,'o'); identityLine
+xlabel('Center'); ylabel('Green wall');
+
+ieNewGraphWin; plot(rgb2,rgb3,'o'); identityLine
+xlabel('Red Wall'); ylabel('Green wall');
