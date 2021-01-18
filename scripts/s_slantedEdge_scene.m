8dd83e6ae16d12710fd475ed4ed46fd96301a875
ZhengLyu <zhenglyu@stanford.edu>
2021-01-18 10:18:49 -0800

    modified mcc pos rendeing specification

--- scripts/s_slantedEdge_scene.m
+++ scripts/s_slantedEdge_scene.m
@@ -34,11 +34,12 @@ T2 = thisR.set('asset', rootST2.name, 'world translate', [0.0525 0 -0.10]); % 16
 %}
 
 %% Specify rendering settings
-thisR.set('film resolution',[2048 2048]);
+thisR.set('film resolution',[4032 3024]);
 nRaysPerPixel = 2048;
 thisR.set('rays per pixel',nRaysPerPixel);
-thisR.set('nbounces',5);
-
+thisR.set('nbounces', 5);
+thisR.set('fov', 50);
+thisR.set('film diagonal', 7.056); % mm
 %% Set first focus (in the back)
 thisR.set('focus distance', 0.5);
 % {
@@ -46,7 +47,7 @@ thisR.set('focus distance', 0.5);
 piWrite(thisR);
 % Render
 [scene, result] = piRender(thisR, 'render type', 'radiance', 'scale illuminance', false);
-sceneName = sprintf('CBLens_slantedEdge_scene');
+sceneName = sprintf('CBLens_slantedEdge_scene_correct');
 scene = sceneSet(scene, 'name', sceneName);
 sceneWindow(scene);
 sceneSet(scene, 'gamma', 0.5);
